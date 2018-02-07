3e966dc7f4909d26ad990113f6f691880b1723d9
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-07 17:14:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@
 def prime?(i)
   range = (2..(integer -1)).to_a
 range.any? do |n|
-  if i <= 1
+  if i == 2 || i < 2 && i % 2 == 1
+    return true
+  elsif i <= 1
     return false
-elsif i == 2 || i < 2 && i % 2 == 1
-  return true
 else
   return false
   end
