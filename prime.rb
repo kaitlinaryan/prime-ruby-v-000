af022d04d58f746a0158d5cc66d48f98048518e9
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-07 17:02:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,14 @@
-def prime?(integer)
-  range = (2...max).to_a
-  range.each do |i|
-  i < 2 && i % 2 == 1
+def prime?(i)
+  if i <= 1
+    return false
+elsif i == 2
+  return true
+elsif i < 2 && i % 2 == 1
+  return true
   end
 end
+
+
+
+#range = (2...max).to_a
+#range.each do |i|
