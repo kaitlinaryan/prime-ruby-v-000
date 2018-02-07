7694195c46478edcc953eaadfa6e0c5ebbb13de4
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-07 17:10:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,5 @@
-def prime?(i)
-  if i <= 1
-    return false
-elsif i == 2
-  return true
-elsif i < 2 && i % 2 == 1
-  return true
-else
-  return false
-  end
+
+def prime?(integer)
+  range = (2..(integer -1)).to_a
+(range.any? {|number| integer % number == 0} || integer == 0 || integer == 1) ? false : true
 end
-
-
-
-#range = (2...max).to_a
-#range.each do |i|
