5bb9c86a9d5456ad7db524247b1bcb7b815bac13
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-07 17:27:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(i)
-if i == 1
+    if i <= 1
   return false
-elsif i == 2
-  return true
+elsif i == 2 
+       true
 elsif (2..(i/2)).any? {|prime| i % prime == 0 }
   return false
 else
   return true
-end
+   end
 end
